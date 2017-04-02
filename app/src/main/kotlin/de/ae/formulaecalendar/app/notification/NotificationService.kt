d5d54c491e521d925862db7f92658b29df30e683
aeilers <a.eilers@hs-osnabrueck.de>
2017-04-02 17:25:57 +0200

    fix TypeCastException in NotoficationService

--- app/src/main/kotlin/de/ae/formulaecalendar/app/notification/NotificationService.kt
+++ app/src/main/kotlin/de/ae/formulaecalendar/app/notification/NotificationService.kt
@@ -7,6 +7,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import com.google.firebase.crash.FirebaseCrash
 import com.jakewharton.threetenabp.AndroidThreeTen
 import de.ae.formulaecalendar.app.R
 import de.ae.formulaecalendar.app.view.details.DetailsActivity
@@ -19,36 +20,43 @@ import org.threeten.bp.format.DateTimeFormatter
  */
 class NotificationService : IntentService("NotificationService") {
 
+
     override fun onHandleIntent(intent: Intent?) {
 
         //initialize ThreeTen
         AndroidThreeTen.init(this)
 
-        val race = intent!!.getSerializableExtra("race") as CalendarDatum ?: return
+        val race = intent?.getSerializableExtra("race") as CalendarDatum?
 
-        //create Notification
-        val format = getString(R.string.format_date) + ' ' + getString(R.string.format_time)
-        val zdt = race?.raceStart?.withZoneSameInstant(ZoneId.systemDefault())
-        val time = zdt.format(DateTimeFormatter.ofPattern(format))
+        if (race != null) {
 
-        val newIntent = Intent(this, DetailsActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(this, 0, newIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            //create Notification
+            val format = getString(R.string.format_date) + ' ' + getString(R.string.format_time)
+            val zdt = race.raceStart.withZoneSameInstant(ZoneId.systemDefault())
+            val time = zdt.format(DateTimeFormatter.ofPattern(format))
 
-        val builder = Notification.Builder(this)
-                .setContentTitle(race.raceName)
-                .setContentText(String.format(getString(R.string.noti_content), time))
-                .setContentIntent(pendingIntent)
-                .setSmallIcon(R.mipmap.icon_bw)
+            val newIntent = Intent(this, DetailsActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(this, 0, newIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            builder.setCategory(CATEGORY_ALARM)
+            val builder = Notification.Builder(this)
+                    .setContentTitle(race.raceName)
+                    .setContentText(String.format(getString(R.string.noti_content), time))
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.mipmap.icon_bw)
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                builder.setCategory(CATEGORY_ALARM)
+            }
+
+            val notification = builder.build()
+            notification.flags = notification.flags or Notification.FLAG_AUTO_CANCEL
+
+            //show Notification
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(race.hashCode(), notification)
+
+        } else {
+            FirebaseCrash.log("Cannot laod race from intent. Intent: " + intent?.toString())
         }
-
-        val notification = builder.build()
-        notification.flags = notification.flags or Notification.FLAG_AUTO_CANCEL
-
-        //show Notification
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(race.hashCode(), notification)
     }
 }
