9d216704c02ef20049ce6b0188b9173ab8959ba2
Alexander Eilers <a.eilers@hs-osnabrueck.de>
2018-11-25 14:59:47 +0100

    remove ThreeTen Android Backport from ressource list

--- app/src/main/kotlin/de/ae/formulaecalendar/app/resource/LocalResourceStore.kt
+++ app/src/main/kotlin/de/ae/formulaecalendar/app/resource/LocalResourceStore.kt
@@ -100,26 +100,6 @@ object LocalResourceStore : ResourceStore {
         })
 
         resources.add(Resource().apply {
-            name = "threeten"
-            title = "ThreeTen Android Backport"
-            description = "ThreeTen Android Backport is used in this app"
-            license = "Copyright (C) 2015 Jake Wharton\n" +
-                    "\n" +
-                    "Licensed under the Apache License, Version 2.0 (the \"License\"); " +
-                    "you may not use this file except in compliance with the License. " +
-                    "You may obtain a copy of the License at\n" +
-                    "\n" +
-                    "http://www.apache.org/licenses/LICENSE-2.0\n" +
-                    "\n" +
-                    "Unless required by applicable law or agreed to in writing, software " +
-                    "distributed under the License is distributed on an \"AS IS\" BASIS, " +
-                    "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. " +
-                    "See the License for the specific language governing permissions and " +
-                    "limitations under the License."
-            uri = "https://github.com/JakeWharton/ThreeTenABP"
-        })
-
-        resources.add(Resource().apply {
             name = "2016020100" //Hong Kong
             title = "Overlook Hong Kong Island north coast, Victoria Harbour and Kowloon from " +
                     "middle section of Lugard Road at daytime (enlarged version and better " +
